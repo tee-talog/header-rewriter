7734294125d1469947aba537984d33b327a2f2ee
talog <tee.talog@gmail.com>
2023-04-15 19:45:52 +0900

    feat: 任意の URL へのリクエストをブロックする仮実装作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,74 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [domain, setDomain] = useState("")
+  const inputDomain = (e: any) => {
+    setDomain(e.target.value)
+  }
+
+  useEffect(() => {
+    updateNowBlockedDomains()
+  }, [])
+
+  const [nowBlockedRules, setNowBlockedRules] = useState<
+    chrome.declarativeNetRequest.Rule[]
+  >([])
+
+  const updateNowBlockedDomains = async () => {
+    const rules = await chrome.declarativeNetRequest.getDynamicRules()
+    setNowBlockedRules(rules)
+  }
+
+  const addRule = (domainRegExp: string) => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [
+        {
+          action: { type: chrome.declarativeNetRequest.RuleActionType.BLOCK },
+          condition: {
+            regexFilter: domainRegExp,
+          },
+          id: 2,
+        },
+      ],
+    })
+    updateNowBlockedDomains()
+  }
+
+  const removeRule = (id: number) => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [id],
+    })
+    updateNowBlockedDomains()
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <div className="App" style={{ width: "600px" }}>
+      <div>
+        <form>
+          <label>
+            Domain <input type="text" value={domain} onChange={inputDomain} />
+          </label>
+          <button type="button" onClick={() => addRule(domain)}>
+            Add Block Domain
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        </form>
+      </div>
+
+      <div>
+        <ul>
+          {nowBlockedRules.map((r) => (
+            <li key={r.id}>
+              <label>
+                {JSON.stringify(r)}
+                <button type="button" onClick={() => removeRule(r.id)}>
+                  Remove
+                </button>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }

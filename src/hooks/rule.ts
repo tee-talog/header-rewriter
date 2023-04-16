29d33770a62c41b10fc40c36bfaad92f1dbb408a
talog <tee.talog@gmail.com>
2023-04-16 22:58:58 +0900

    feat: 複数のルールを設定するための関数を追加

--- src/hooks/rule.ts
+++ src/hooks/rule.ts
@@ -26,8 +26,48 @@ export const addRule = async (
   })
 }
 
+type HeaderSetRule = {
+  id: number
+  regexFilter: string
+  keyValue: {
+    header: string
+    value: string
+  }[]
+}
+
+export const addHeaderSetRules = async (rules: HeaderSetRule[]) => {
+  const addRules = rules.map<chrome.declarativeNetRequest.Rule>(
+    ({ id, regexFilter, keyValue }) => {
+      const requestHeaders = keyValue.map(({ header, value }) => ({
+        operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+        header,
+        value,
+      }))
+
+      return {
+        id,
+        condition: {
+          regexFilter,
+        },
+        action: {
+          type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+          requestHeaders,
+        },
+      }
+    },
+  )
+
+  await chrome.declarativeNetRequest.updateDynamicRules({ addRules })
+}
+
 export const removeRule = async (id: number) => {
   await chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: [id],
+  })
+}
+
+export const removeRules = async (ids: number[]) => {
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: ids,
   })
 }

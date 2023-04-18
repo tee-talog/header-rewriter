bc03ae533a76efcf71b3cecab1167760c0592fa0
talog <tee.talog@gmail.com>
2023-04-18 09:58:17 +0900

    feat: オプション画面の HTML を仮組み

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -2,7 +2,60 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
-  return <div>option</div>
+  return (
+    <>
+      <header>
+        <h1>options</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>rules</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>rule name</td>
+                <td>regexp</td>
+                <td>
+                  <ul>
+                    <li>header, value</li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+
+        <section>
+          <h2>add rule</h2>
+          <form>
+            <label>
+              rule name
+              <input type="text" />
+            </label>
+            <label>
+              regexp
+              <input type="text" />
+            </label>
+            <ul>
+              <li>
+                <label>
+                  header
+                  <input type="text" />
+                </label>
+                <label>
+                  value
+                  <input type="text" />
+                </label>
+                <button type="button">+</button>
+              </li>
+            </ul>
+            <button type="submit">add</button>
+          </form>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App

4324a2e71b9e98a9a4c8aa1bcdeb40644692a9c4
talog <tee.talog@gmail.com>
2023-04-18 22:03:20 +0900

    feat: RuleList が options を受け取れるようにした

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -3,24 +3,26 @@ import "./App.css"
 import RuleList from "./RuleList"
 import AddRuleForm from "./AddRuleForm"
 
-const App = () => (
-  <>
-    <header>
-      <h1>options</h1>
-    </header>
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>options</h1>
+      </header>
 
-    <main>
-      <section>
-        <h2>rules</h2>
-        <RuleList />
-      </section>
+      <main>
+        <section>
+          <h2>rules</h2>
+          <RuleList rules={[]} />
+        </section>
 
-      <section>
-        <h2>add rule</h2>
-        <AddRuleForm />
-      </section>
-    </main>
-  </>
-)
+        <section>
+          <h2>add rule</h2>
+          <AddRuleForm />
+        </section>
+      </main>
+    </>
+  )
+}
 
 export default App
--- src/options/RuleList.tsx
+++ src/options/RuleList.tsx
@@ -1,17 +1,62 @@
-const RuleList = () => (
-  <table>
-    <tbody>
-      <tr>
-        <td>rule name</td>
-        <td>regexp</td>
-        <td>
-          <ul>
-            <li>header, value</li>
-          </ul>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-)
+import React from "react"
+
+// TODO 仮
+export type HeaderSetOption = {
+  type: "set"
+  name: string
+  regexFilter: string
+  keyValue: {
+    header: string
+    value: string
+  }[]
+}
+
+export type HeaderRemoveOption = {
+  type: "remove"
+  name: string
+  regexFilter: string
+  headers: string[]
+}
+
+type HeaderRewriteOption = HeaderSetOption | HeaderRemoveOption
+
+const RuleList: React.FC<{ rules: HeaderRewriteOption[] }> = ({ rules }) => {
+  const ListItemSet: React.FC<{ rule: HeaderSetOption }> = ({ rule }) => (
+    <>
+      {rule.keyValue.map(({ header, value }) => (
+        <li key={header}>
+          set: {header}: {value}
+        </li>
+      ))}
+    </>
+  )
+
+  const ListItemRemove: React.FC<{ rule: HeaderRemoveOption }> = ({ rule }) => (
+    <>
+      {rule.headers.map((header) => (
+        <li key={header}>remove: {header}</li>
+      ))}
+    </>
+  )
+
+  return (
+    <table>
+      <tbody>
+        {rules.map((rule) => (
+          <tr key={rule.regexFilter}>
+            <td>rule name: {rule.name}</td>
+            <td>regexp: {rule.regexFilter}</td>
+            <td>
+              <ul>
+                {rule.type === "set" && <ListItemSet rule={rule} />}
+                {rule.type === "remove" && <ListItemRemove rule={rule} />}
+              </ul>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default RuleList

6db32c3d8a334d3b49731a363b74ce2cfac0aa8e
talog <tee.talog@gmail.com>
2023-04-18 22:17:47 +0900

    feat: RHF の導入

--- src/options/AddOptionForm.tsx
+++ src/options/AddOptionForm.tsx
@@ -1,28 +1,48 @@
-const AddRuleForm = () => (
-  <form>
-    <label>
-      rule name
-      <input type="text" />
-    </label>
-    <label>
-      regexp
-      <input type="text" />
-    </label>
-    <ul>
-      <li>
-        <label>
-          header
-          <input type="text" />
-        </label>
-        <label>
-          value
-          <input type="text" />
-        </label>
-        <button type="button">+</button>
-      </li>
-    </ul>
-    <button type="submit">add</button>
-  </form>
-)
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type Inputs = {
+  ruleName: string
+  regExp: string
+  header: string
+  value: string
+}
+
+const AddRuleForm: React.FC<{
+  onSubmit: SubmitHandler<Inputs>
+}> = ({ onSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        rule name
+        <input type="text" {...register("ruleName", { required: true })} />
+      </label>
+      <label>
+        regexp
+        <input type="text" {...register("regExp", { required: true })} />
+      </label>
+      <ul>
+        <li>
+          <label>
+            header
+            <input type="text" {...register("header", { required: true })} />
+          </label>
+          <label>
+            value
+            <input type="text" {...register("value", { required: true })} />
+          </label>
+          <button type="button">+</button>
+        </li>
+      </ul>
+      <button type="submit">add</button>
+    </form>
+  )
+}
 
 export default AddRuleForm

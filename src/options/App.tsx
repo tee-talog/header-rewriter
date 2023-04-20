ee8626cdaadcb1f95f9c1c6ed8835d8caf6214d8
talog <tee.talog@gmail.com>
2023-04-20 22:10:05 +0900

    feat: storage からオプションを読み込んで表示する

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import OptionList from "./OptionList"
 import AddOptionForm from "./AddOptionForm"
 import { SubmitHandler } from "react-hook-form"
+import { loadOptions } from "../hooks/storage"
+import { HeaderRewriteOption } from "../types"
 
 // TODO 仮
 type Inputs = {
@@ -16,9 +18,21 @@ type Inputs = {
 }
 
 const App = () => {
+  // TODO 子側でバリデーション。zod を使うと型変換ができるみたい
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data)
   }
+
+  const [options, setOptions] = useState<HeaderRewriteOption[]>([])
+
+  const load = async () => {
+    const opt = await loadOptions()
+    setOptions(opt)
+  }
+  useEffect(() => {
+    load()
+  }, [])
+
   return (
     <>
       <header>
@@ -28,7 +42,7 @@ const App = () => {
       <main>
         <section>
           <h2>options</h2>
-          <OptionList options={[]} />
+          <OptionList options={options} />
         </section>
 
         <section>

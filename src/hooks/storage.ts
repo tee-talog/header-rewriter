1174c4b72043f2453f323eb687e55de126303170
talog <tee.talog@gmail.com>
2023-04-20 22:18:39 +0900

    fix: storage が空のときに発生するエラーを解消

--- src/hooks/storage.ts
+++ src/hooks/storage.ts
@@ -5,7 +5,9 @@ const HEADER_REWRITE_OPTIONS_KEY = "options"
 export const saveOptions = (options: HeaderRewriteOption[]) =>
   chrome.storage.local.set({ [HEADER_REWRITE_OPTIONS_KEY]: options })
 
-export const loadOptions = () =>
-  chrome.storage.local.get(HEADER_REWRITE_OPTIONS_KEY) as unknown as Promise<
+export const loadOptions = async () => {
+  const value = await chrome.storage.local.get(HEADER_REWRITE_OPTIONS_KEY)
+  return (value[HEADER_REWRITE_OPTIONS_KEY] ?? []) as Promise<
     HeaderRewriteOption[]
   >
+}

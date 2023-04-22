2daa1964687dedf5b3ca58515ff9d1e0eab19ff2
talog <tee.talog@gmail.com>
2023-04-23 01:01:00 +0900

    fix: 変数名の変更漏れ

--- src/hooks/storage.ts
+++ src/hooks/storage.ts
@@ -1,11 +1,11 @@
 import { HeaderRewriteOption } from "../types"
 
-const RULE_KEY = "RULE_KEY"
+const OPTION_KEY = "OPTION_KEY"
 
-export const saveOptions = (rules: HeaderRewriteOption[]) =>
-  chrome.storage.local.set({ [RULE_KEY]: rules })
+export const saveOptions = (options: HeaderRewriteOption[]) =>
+  chrome.storage.local.set({ [OPTION_KEY]: options })
 
 export const loadOptions = async (): Promise<HeaderRewriteOption[]> => {
-  const value = await chrome.storage.local.get(RULE_KEY)
-  return value[RULE_KEY] as HeaderRewriteOption[]
+  const value = await chrome.storage.local.get(OPTION_KEY)
+  return value[OPTION_KEY] as HeaderRewriteOption[]
 }

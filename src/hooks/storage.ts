1008185b51546a26bc11336011bda29c6e6a24dc
talog <tee.talog@gmail.com>
2023-04-22 22:14:45 +0900

    refactor: 不要処理を削除

--- src/hooks/storage.ts
+++ src/hooks/storage.ts
@@ -1,18 +1,5 @@
-import { HeaderRewriteOption, Rule } from "../types"
+import { Rule } from "../types"
 
-const HEADER_REWRITE_OPTIONS_KEY = "options"
-
-export const saveOptions = (options: HeaderRewriteOption[]) =>
-  chrome.storage.local.set({ [HEADER_REWRITE_OPTIONS_KEY]: options })
-
-export const loadOptions = async () => {
-  const value = await chrome.storage.local.get(HEADER_REWRITE_OPTIONS_KEY)
-  return (value[HEADER_REWRITE_OPTIONS_KEY] ?? []) as Promise<
-    HeaderRewriteOption[]
-  >
-}
-
-//
 const RULE_KEY = "RULE_KEY"
 
 export const saveRules = (rules: Rule[]) =>

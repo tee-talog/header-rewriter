e1d789312b5c1b856bb55b7a6b0453c75e84662c
talog <tee.talog@gmail.com>
2023-04-22 21:38:07 +0900

    feat: 設定をストレージに保存・取得

--- src/hooks/storage.ts
+++ src/hooks/storage.ts
@@ -1,4 +1,4 @@
-import { HeaderRewriteOption } from "../types"
+import { HeaderRewriteOption, Rule } from "../types"
 
 const HEADER_REWRITE_OPTIONS_KEY = "options"
 
@@ -10,4 +10,15 @@ export const loadOptions = async () => {
   return (value[HEADER_REWRITE_OPTIONS_KEY] ?? []) as Promise<
     HeaderRewriteOption[]
   >
+}
+
+//
+const RULE_KEY = "RULE_KEY"
+
+export const saveRules = (rules: Rule[]) =>
+  chrome.storage.local.set({ [RULE_KEY]: rules })
+
+export const loadRules = async (): Promise<Rule[]> => {
+  const value = await chrome.storage.local.get(RULE_KEY)
+  return value as Rule[]
 }
--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,10 +1,51 @@
 import "./App.css"
 import RuleList from "./RuleList"
-import AddRuleForm from "./AddRuleForm"
+import AddRuleForm, { FormInputs } from "./AddRuleForm"
+import { saveRules } from "../hooks/storage"
+import { Rule } from "../types"
+import { useState } from "react"
+
+const convertToRule = (formData: FormInputs): Rule => {
+  // TODO
+  const id = 0
+  const operation =
+    formData.type === "set"
+      ? chrome.declarativeNetRequest.HeaderOperation.SET
+      : chrome.declarativeNetRequest.HeaderOperation.REMOVE
+
+  return {
+    id,
+    name: formData.ruleName,
+    rule: {
+      id,
+      action: {
+        type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+        requestHeaders: [
+          {
+            operation,
+            header: formData.key,
+            value: formData.value,
+          },
+        ],
+      },
+      condition: {
+        regexFilter: formData.regExp,
+      },
+    },
+  }
+}
 
 const App = () => {
-  const onRemove = () => {}
-  const onSubmit = () => {}
+  const [rules, setRules] = useState<Rule[]>([])
+
+  const onRemove = (id: number) => {
+    setRules(rules.filter((rule) => rule.id !== id))
+  }
+
+  const onSubmit = (formData: FormInputs) => {
+    const rule = convertToRule(formData)
+    saveRules([rule])
+  }
 
   return (
     <>
@@ -15,7 +56,7 @@ const App = () => {
       <main>
         <section>
           <h2>options</h2>
-          <RuleList rules={[]} onRemove={onRemove} />
+          <RuleList rules={rules} onRemove={onRemove} />
         </section>
 
         <section>

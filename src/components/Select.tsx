c577d262593832e9bbe9d1670a09dbba1dcdbe29
talog <tee.talog@gmail.com>
2023-05-21 23:55:05 +0900

    fix: Select を forwardRef 化

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,30 +1,29 @@
 import clsx from "clsx"
-import { ComponentProps } from "react"
+import { ComponentProps, forwardRef } from "react"
 
-const Select: React.FC<ComponentProps<"select">> = ({
-  className,
-  children,
-  ...props
-}) => {
-  return (
-    <select
-      className={clsx(
-        className,
-        "px-2",
-        "py-1",
-        "border",
-        "rounded",
-        "border-gray-500",
-        "focus:bg-gray-100",
-        "min-w-[5rem]",
-        "text-base",
-        "text-gray-900",
-      )}
-      {...props}
-    >
-      {children}
-    </select>
-  )
-}
+const Select = forwardRef<HTMLSelectElement, ComponentProps<"select">>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <select
+        ref={ref}
+        className={clsx(
+          className,
+          "px-2",
+          "py-1",
+          "border",
+          "rounded",
+          "border-gray-500",
+          "focus:bg-gray-100",
+          "min-w-[5rem]",
+          "text-base",
+          "text-gray-900",
+        )}
+        {...props}
+      >
+        {children}
+      </select>
+    )
+  },
+)
 
 export default Select
